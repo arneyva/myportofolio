e599a4b483bf9d05a8bdd33d531567029b37f867
arneyva <princethereal1416@gmail.com>
2023-12-10 08:48:15 +0700

    hero part1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Hero from '@/components/main/Hero'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <a href="">ini buatanya akun arneyva</a>
+    <main className="h-full w-full">
+      {/* <a href="">ini buatanya akun arneyva</a> */}
+      <div className='flex flex-col h-[850px] gap-20'>
+        <Hero />
+      </div>
     </main>
   )
 }
